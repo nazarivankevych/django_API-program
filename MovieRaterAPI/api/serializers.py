a20200b81d3d4e62476b3219d5034654870a52a4
NazarIvankevych <nazar.ivankevych@gmail.com>
2021-11-17 10:04:04 +0200

    Add users
    Try to give users an opportunity to register and had got its token. Need to fix problem with new token generating.

--- MovieRaterAPI/api/serializers.py
+++ MovieRaterAPI/api/serializers.py
@@ -1,6 +1,25 @@
-from django.db.models import fields
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 from .models import Movie, Rating
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True, 
+                'required': True
+                }
+            }
+        
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            token = Token.objects.create(user=user)
+            print("XXXXX - ", token)
+            return user
+
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
--- MovieRaterAPI/api/views.py
+++ MovieRaterAPI/api/views.py
@@ -2,13 +2,19 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
 from .models import Movie, Rating
-from .serializers import MovieSerializer, RatingSerializer
+from .serializers import MovieSerializer, RatingSerializer, UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication, )
     
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
@@ -16,9 +22,7 @@ class MovieViewSet(viewsets.ModelViewSet):
             # select title with id from db
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
-            # user = request.user
-            user = User.objects.get(id=1)
-            print("User: ", user.username)
+            user = request.user
             # create rating for movie from user
             try:
                 rating = Rating.objects.get(user=user.id, movie=movie.id)
@@ -41,3 +45,4 @@ class MovieViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication, )
