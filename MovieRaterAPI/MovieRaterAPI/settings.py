224b322ab899203ed6c391e660ba904af6d8846c
NazarIvankevych <nazar.ivankevych@gmail.com>
2021-12-01 21:53:29 +0200

    Working with db
    Add cors headers to avoid conflicts with browser security

--- MovieRaterAPI/MovieRaterAPI/settings.py
+++ MovieRaterAPI/MovieRaterAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wk@+46s5sb&p_s*z21v3b9i&z(_pcl%hzqdp!lk%$)^)p#+oi='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -39,17 +39,23 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'api'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+            "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'MovieRaterAPI.urls'
@@ -126,3 +132,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
